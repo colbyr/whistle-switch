b5e802cc82a1eefd542537cd4bcdb4ca2f06a223
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2022-01-16 12:44:26 -0500

    correctly resolve the script through symlinks

--- start.sh
+++ start.sh
@@ -1,15 +1,25 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 trap "kill 0" EXIT
 
-DIR=$(dirname "$0")
+# finds out where the script actually lives
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+if ([ -h "${SCRIPT_PATH}" ]); then
+  while([ -h "${SCRIPT_PATH}" ]); do cd "$(dirname "$SCRIPT_PATH")";
+  SCRIPT_PATH=$(readlink "${SCRIPT_PATH}"); done
+fi
+cd "$(dirname ${SCRIPT_PATH})" > /dev/null
+SCRIPT_PATH=$(pwd);
+popd  > /dev/null
 
 PORT=${WHISTLE_SWITCH_LISTENER_PORT:=6767}
 
 echo "🚧 starting listener..."
-$DIR/node_modules/.bin/vite --port $PORT $DIR > /dev/null 2>&1 &
+$SCRIPT_PATH/node_modules/.bin/vite --port $PORT $SCRIPT_PATH > /dev/null 2>&1 &
 
 sleep 1
 
 echo "🦾 starting controller!"
-node "$DIR/src/controller.js"
+node "$SCRIPT_PATH/src/controller.js"
 
